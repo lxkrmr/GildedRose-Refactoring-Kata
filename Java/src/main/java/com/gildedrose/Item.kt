1d224f1580aa7fe277ba23de56df6d11ea093024
Alex <lxkrmr@icloud.com>
2022-06-18 15:39:22 +0200

    Introduced subclasses

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -9,55 +9,79 @@ open class Item(
     override fun toString() = "$name, $sellIn, $quality"
 }
 
-class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
+open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
     fun update() {
-        if (name != "Aged Brie"
-            && name != "Backstage passes to a TAFKAL80ETC concert"
-        ) {
-            if (quality > 0) {
-                if (name != "Sulfuras, Hand of Ragnaros") {
-                    quality = quality - 1
-                }
-            }
-        } else {
-            if (quality < 50) {
-                quality = quality + 1
-                if (name
-                    == "Backstage passes to a TAFKAL80ETC concert"
-                ) {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1
-                        }
-                    }
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1
-                        }
-                    }
-                }
-            }
+        update1()
+        update2()
+        update3()
+    }
+
+    protected open fun update1() {
+        if (quality > 0) {
+            quality -= 1
         }
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn = sellIn - 1
+    }
+
+    protected open fun update2() {
+        sellIn -= 1
+    }
+
+    protected open fun update3() {
+        if (sellIn < 0 && quality > 0) {
+            quality = quality - 1
         }
-        if (sellIn < 0) {
-            if (name != "Aged Brie") {
-                if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (quality > 0) {
-                        if (name != "Sulfuras, Hand of Ragnaros") {
-                            quality = quality - 1
-                        }
-                    }
-                } else {
-                    quality = quality - quality
-                }
-            } else {
+    }
+}
+
+class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
+    override fun update1() {
+        if (quality < 50) {
+            quality += 1
+        }
+    }
+
+    override fun update3() {
+        if (sellIn < 0 && quality < 50) {
+            quality += 1
+        }
+    }
+}
+
+class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
+    override fun update1() {
+        if (quality < 50) {
+            quality += 1
+            if (sellIn < 11) {
                 if (quality < 50) {
-                    quality = quality + 1
+                    quality += 1
+                }
+            }
+            if (sellIn < 6) {
+                if (quality < 50) {
+                    quality += 1
                 }
             }
         }
+    }
+
+    override fun update3() {
+        if (sellIn < 0) {
+            quality = 0
+        }
+    }
+}
+
+class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
+    override fun update1() {
+        // sulfuras is not allowed to update anything here
+    }
+
+    override fun update2() {
+        // sulfuras is not allowed to update anything here
+    }
+
+    override fun update3() {
+        // sulfuras is not allowed to update anything here
     }
 }
 
