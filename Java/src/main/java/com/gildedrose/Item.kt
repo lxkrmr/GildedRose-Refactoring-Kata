6b83d68cdfd9db7d25584598f7ab2ab34e2b369c
Alex <lxkrmr@icloud.com>
2022-06-19 12:02:18 +0200

    Extract common policies

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -9,24 +9,53 @@ open class Item(
     override fun toString() = "$name, $sellIn, $quality"
 }
 
-class BaseItem(
-    name: String,
-    sellIn: Int,
-    quality: Int,
-    private val aging: () -> Int = { 1 },
-    private val degradation: (Int, Int) -> Int = { sellIn: Int, _: Int ->
+object Aging {
+    val standard: () -> Int = { 1 }
+    val none: () -> Int = { 0 }
+}
+
+object Degradation {
+    val standard: (Int, Int) -> Int = { sellIn, _ ->
         when {
             sellIn < 0 -> 2
             else -> 1
         }
-    },
-    private val saturation: (Int) -> Int = { quality: Int ->
+    }
+    val brie: (Int, Int) -> Int = { sellIn, _ ->
+        when {
+            sellIn < 0 -> -2
+            else -> -1
+        }
+    }
+    val pass: (Int, Int) -> Int = { sellIn, quality ->
+        when {
+            sellIn < 0 -> quality
+            sellIn < 5 -> -3
+            sellIn < 10 -> -2
+            else -> -1
+        }
+    }
+    val none: (Int, Int) -> Int = { _, _ -> 0 }
+}
+
+object Saturation {
+    val standard: (Int) -> Int = { quality ->
         when {
             quality < 0 -> 0
             quality > 50 -> 50
             else -> quality
         }
     }
+    val none: (Int) -> Int = { quality -> quality }
+}
+
+class BaseItem(
+    name: String,
+    sellIn: Int,
+    quality: Int,
+    private val aging: () -> Int = Aging.standard,
+    private val degradation: (Int, Int) -> Int = Degradation.standard,
+    private val saturation: (Int) -> Int = Saturation.standard
 ) : Item(name, sellIn, quality) {
     fun update() {
         sellIn = sellIn - aging()
@@ -39,34 +68,22 @@ fun Sulfuras(name: String, sellIn: Int, quality: Int) = BaseItem(
     name,
     sellIn,
     quality,
-    aging = { 0 },
-    degradation = { _, _ -> 0 },
-    saturation = { it }
+    aging = Aging.none,
+    degradation = Degradation.none,
+    saturation = Saturation.none
 )
 
 fun Brie(name: String, sellIn: Int, quality: Int) = BaseItem(
     name,
     sellIn,
     quality,
-    degradation = { sellIn, _ ->
-        when {
-            sellIn < 0 -> -2
-            else -> -1
-        }
-    }
+    degradation = Degradation.brie
 )
 
 fun Pass(name: String, sellIn: Int, quality: Int) = BaseItem(
     name,
     sellIn,
     quality,
-    degradation = { sellIn, quality ->
-        when {
-            sellIn < 0 -> quality
-            sellIn < 5 -> -3
-            sellIn < 10 -> -2
-            else -> -1
-        }
-    }
+    degradation = Degradation.pass
 )
 
