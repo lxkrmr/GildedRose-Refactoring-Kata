888a22956e625381aca45650ed1361f98b3c10a4
Alex <lxkrmr@icloud.com>
2022-06-19 09:40:17 +0200

    Removed quality from degradation

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -12,15 +12,15 @@ open class Item(
 open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
     fun update() {
         sellIn = sellIn - aging()
-        quality = degradation(sellIn, quality)
+        quality = quality - degradation(sellIn)
         quality = saturation(quality)
     }
 
     protected open fun aging() = 1
 
-    protected open fun degradation(sellIn: Int, quality: Int): Int = when {
-        sellIn < 0 -> quality - 2
-        else -> quality - 1
+    protected open fun degradation(sellIn: Int): Int = when {
+        sellIn < 0 -> 2
+        else -> 1
     }
 
     protected open fun saturation(quality: Int) = when {
@@ -31,25 +31,25 @@ open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn
 }
 
 class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degradation(sellIn: Int, quality: Int): Int = when {
-        sellIn < 0 -> quality + 2
-        else -> quality + 1
+    override fun degradation(sellIn: Int): Int = when {
+        sellIn < 0 -> - 2
+        else -> - 1
     }
 }
 
 class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degradation(sellIn: Int, quality: Int): Int = when {
-        sellIn < 0 -> 0
-        sellIn < 5 -> quality + 3
-        sellIn < 10 -> quality + 2
-        else -> quality + 1
+    override fun degradation(sellIn: Int): Int = when {
+        sellIn < 0 -> quality
+        sellIn < 5 -> - 3
+        sellIn < 10 -> - 2
+        else -> - 1
     }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun aging(): Int = 0
 
-    override fun degradation(sellIn: Int, quality: Int): Int = quality
+    override fun degradation(sellIn: Int): Int = 0
 
     override fun saturation(quality: Int): Int = quality
 }
