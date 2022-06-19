3d0869e9099e1eab1cc17e57b8a8d3191055bac8
Alex <lxkrmr@icloud.com>
2022-06-19 13:10:24 +0200

    Merged BaseItem into Item

--- Java/src/test/java/com/gildedrose/GildedRoseTest.kt
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.kt
@@ -1,17 +1,19 @@
-package com.gildedrose;
+package com.gildedrose
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class GildedRoseTest {
-
+internal class GildedRoseTest {
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.getItems()[0].getName());
-    }
+    fun foo() {
+        // given
+        val items = arrayOf(Item("foo", 0, 0))
+        val app = GildedRose(items)
 
+        // when
+        app.updateQuality()
+
+        // then
+        assertEquals("foo", app.items[0].name)
+    }
 }
